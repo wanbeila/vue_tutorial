8c9cf5adf96168865f9118b66b38366bc1dda903
wbl <wbl>
2019-03-31 00:03:59 +0800

    sss

--- vue_project/Travel/src/main.js
+++ vue_project/Travel/src/main.js
@@ -1,6 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+// 会自动查找App的文件
+// .vue为单文件组件
 import App from './App'
 import router from './router'
 
@@ -10,6 +12,9 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  // 省略写法 键和值名字一样
   components: { App },
   template: '<App/>'
 })
+
+// 路由就是根据url的不同，返回不同的内容
